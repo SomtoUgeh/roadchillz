559dfd351db4fc53598db1a82283faa6bc281d3d
coderaayush <contact@aayushmamgain.com>
2021-08-04 06:17:52 +0100

    models and migrations

--- roadchillz/models.py
+++ roadchillz/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+class Category(models.Model):
+    NAME_MAX_LENGTH = 64
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def save (self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    NAME_MAX_LENGTH = 64
+    IMAGE_URL_MAX_LENGTH = 128
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
+    long = models.FloatField()
+    lat = models.FloatField()
+    location_name = models.CharField(max_length=NAME_MAX_LENGTH)
+    open_time = models.DateTimeField()
+    close_time = models.DateTimeField()
+    image_url = models.CharField(max_length=IMAGE_URL_MAX_LENGTH)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    NAME_MAX_LENGTH = 64
+    IMAGE_URL_MAX_LENGTH = 128
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
+    image_url = models.CharField(max_length=IMAGE_URL_MAX_LENGTH)
+    price = models.BigIntegerField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Cuisine(models.Model):
+    NAME_MAX_LENGTH = 64
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
+    item = models.ManyToManyField(Restaurant)
+    
+    def __str__(self):
+        return self.name
--- roadchillz/views.py
+++ roadchillz/views.py
@@ -1,9 +1,24 @@
+from roadchillz.models import Restaurant
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from roadchillz.models import Restaurant, Category, Item
 
 def index(request):
     return HttpResponse("Hello world")
 
 
 # Create your views here.
+
+def list_restaurants(request):
+    context_dict = {}
+    try:
+        # checking related data
+        # restaurants = Restaurant.objects.get(id=1)
+        # print(restaurants.category)
+
+        restaurants = Restaurant.objects.all()
+        context_dict['restaurants'] = restaurants
+    except Category.DoesNotExist:
+        context_dict['restaurants'] = None
+    print(context_dict)
+    return render(request, 'restaurants.html', context=context_dict)
