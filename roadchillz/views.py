086de278039b3a0685ce14c802821743d05cdab4
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2021-08-04 15:08:18 +0100

    add authentication pages

--- roadchillz/views.py
+++ roadchillz/views.py
@@ -1,13 +1,14 @@
 from roadchillz.models import Restaurant
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from roadchillz.models import Restaurant, Category, Item
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
 
 def index(request):
     return render(request, 'roadchillz/index.html')
 
-
-# Create your views here.
 
 def list_restaurants(request):
     context_dict = {}
@@ -22,3 +23,27 @@ def list_restaurants(request):
         context_dict['restaurants'] = None
     print(context_dict)
     return render(request, 'restaurants.html', context=context_dict)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('roadchillz:index'))
+            else:
+                return HttpResponse("Your Roadchillz account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'roadchillz/login.html')
+
+
+def signup(request):
+    return render(request, 'roadchillz/signup.html')
