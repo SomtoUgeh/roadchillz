58570b9ea32b60007f8416c6f05e4ac37fe3e8d2
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2021-08-06 10:10:57 +0100

    cleanups | add like fn to single restaurant

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,25 @@
-$( document ).ready(function() {
-    $('.timepicker').timepicker({
-        timeFormat: 'h:i a',
-        interval: 60,
-        defaultTime: '8',
-        startTime: '00:00',
-        dynamic: false,
-        dropdown: true,
-        scrollbar: true
-    });
+$(document).ready(function () {
+	$('.timepicker').timepicker({
+		timeFormat: 'h:i a',
+		interval: 60,
+		defaultTime: '8',
+		startTime: '00:00',
+		dynamic: false,
+		dropdown: true,
+		scrollbar: true,
+	});
+
+	$('#like_btn').click(function () {
+		let restaurantId;
+		restaurantId = $(this).attr('data-restaurantid');
+
+		$.get(
+			'/roadchillz/like_category/',
+			{ restaurant_id: restaurantId },
+			function (data) {
+				$('#like_count').html(data);
+				$('#like_btn').hide();
+			}
+		);
+	});
 });
