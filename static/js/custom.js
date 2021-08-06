c14572c6a4ab7c707c02d95f0a96523193ed508b
Aayush Mamgain <contact@aayushmamgain.com>
2021-08-06 08:08:01 +0100

    Add restaurant functionality (#25)

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     $('.timepicker').timepicker({
-        timeFormat: 'h:mm p',
+        timeFormat: 'h:i a',
         interval: 60,
         defaultTime: '8',
         startTime: '00:00',
@@ -9,12 +9,3 @@ $( document ).ready(function() {
         scrollbar: true
     });
 });
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 55.8642, lng: -4.2518 },
-    zoom: 10,
-  });
-}
